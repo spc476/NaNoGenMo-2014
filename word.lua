2ac820ffaae336c224ede2d93d7d37e6923087dd
Sean Conner <spc@conman.org>
2014-11-29 19:21:03 -0500

    Pin down final definition of a "word"

--- word.lua
+++ word.lua
@@ -3,6 +3,7 @@ local lpeg = require "lpeg"
 
 local space = lpeg.locale().space
 local alpha = lpeg.locale().alpha
+local digit = lpeg.locale().digit
 local word
 
 if false then
@@ -15,11 +16,44 @@ else
          )
 end
 
-local doc   = lpeg.Ct(word^1)
+local chara = alpha + lpeg.P"'"
+local dash  = lpeg.P"-" * #chara + ""
+local chard = alpha * dash
+            + chara
 
-local FILE = arg[1] or "/home/spc/LINUS/blackbox/writings/docs/neuromancer.txt"
+word = lpeg.C"Mr."
+     + lpeg.C"MR."
+     + lpeg.C"Mrs."
+     + lpeg.C"MRS."
+     + lpeg.C"Dr."
+     + lpeg.C"DR."
+     + lpeg.C"P. S."
+     + lpeg.P"P.S."  / "P. S."
+     + lpeg.C"T. E."
+     + lpeg.P"T.E." / "T. E."
+     + lpeg.C"Gen."
+     + lpeg.C(lpeg.P"No." * space * digit^1)
+     + lpeg.C"N. B."
+     + lpeg.P"N.B." / "N. B."
+     + lpeg.C"H."
+     + lpeg.C"M."     
+     + lpeg.C"O."
+     + lpeg.C"Z."
+     + lpeg.C(chara * chard^0)
 
-local f = io.open(FILE)
+words = (lpeg.P(1) - word)^0 * word
+
+local doc   = lpeg.Ct(words^1)
+
+local FILE = arg[1] or io.stdin
+local f
+
+if type(FILE) == 'string' then
+  f = io.open(FILE)
+else
+  f = FILE
+end
+
 local d = f:read("*a")
 f:close()
 
